69328c93062ec7b8b292e878da0b411dbf3a67e6
ChClaude <christ.tchambila@gmail.com>
2020-12-16 16:05:16 +0200

    user permissions added

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,9 @@
 import uuid
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+from datetime import date
 
 
 class Genre(models.Model):
@@ -80,9 +82,17 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
